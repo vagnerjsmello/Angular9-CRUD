d76920a9ba40d3b2dbbe07b83536e08d246637ff
Vagner Mello <vagnerjsmello@gmail.com>
2020-04-20 22:29:00 +0100

    Add Home Component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,17 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule} from '@angular/material/sidenav'
 import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
 
 import { HeaderComponent } from './components/templates/header/header.component';
 import { FooterComponent } from './components/templates/footer/footer.component';
 import { NavComponent } from './components/templates/nav/nav.component';
+import { HomeComponent } from './components/views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +28,8 @@ import { NavComponent } from './components/templates/nav/nav.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatListModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
